3d21dab585b59ee8733dd83af49f8f11fcdd53d2
jialu <wei.jial@northeastern.edu>
2025-04-23 13:17:25 -0700

    u

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,7 @@
-// server.js
 const express = require("express");
+const cors = require("cors");
 const connectDB = require("./db");
 connectDB();
-const path = require("path");
 const cookieParser = require("cookie-parser");
 
 const userRoutes   = require("./apis/user");
@@ -12,17 +11,18 @@ const scoresRoutes = require("./apis/scores");
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+// Middleware
 app.use(express.json());
 app.use(cookieParser());
+
+app.use(cors({
+  origin: "https://battleship-fronted.onrender.com", 
+  credentials: true,
+}));
 
 app.use("/api/user",   userRoutes);
 app.use("/api/game",   gameRoutes);
 app.use("/api/scores", scoresRoutes);
-
-// app.use(express.static(path.join(__dirname, "../frontend/dist")));
-// app.get("*", (_, res) => {
-//   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-// });
 
 app.get("/", (req, res) => {
   res.send("🎯 Battleship backend is running!");
